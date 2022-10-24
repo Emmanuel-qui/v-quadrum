75f426dc1eab0c7ae7a1de4c7bc7349c4c62bbee
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2022-10-23 21:33:21 -0500

    Iniciando proceso

---
+++ validate/utils.py
@@ -0,0 +1,104 @@
+
+from lxml import etree
+
+from pathlib import Path
+
+from zeep import Client
+import logging
+import base64
+from lxml import etree
+from zeep.plugins import HistoryPlugin
+
+
+
+from django.conf import settings
+
+
+
+
+class Validate:
+
+    def __init__(self, xml_file):
+        self.xml_file = xml_file
+        self.xml_string = self.xml_file.read()
+        self.xml_etree = None
+     
+
+        # Datos de la cuenta
+        self.username = "jquiroz@finkok.com.mx"
+        self.password = "Joseemmanuel_1223"
+        self.url = "https://demo-facturacion.finkok.com/servicios/soap/validation.wsdl"
+
+        self.lista_errores = []
+        # diccionario con los mensajes de validacion.
+        self.response = {}
+        self.success = False
+        self.message = "Error"
+
+        # funcion que da inicio al proceso de validacion.
+        self.start()
+
+    def start(self):
+        self.validate_xsd()
+        if self.success:
+            self.validate_ws()
+            if self.success:
+                self.message = "Comprobante valido"
+       
+                
+     # funcion para validar la estructura de los XML.   
+    def validate_xsd(self):
+        self.success = False
+        print("entra a validacion xsd")
+        try:
+            self.xml_etree = etree.fromstring(self.xml_string, settings.INVOICE_XSD_PARSER)
+            self.success = True
+            self.message = "Estructura valida"
+            self.lista_errores.append(self.message)
+        except Exception as e:
+            self.message = str(e)
+            self.success = True
+
+    # funcion para validar que sea un archivo xml
+    def validate_ext(self):
+        ext = self.xml_file.name.split(".")[-1]
+        if ext.lower() == "xml":
+            self.xml_string = self.xml_file.read()
+            self.success = True
+            self.message = "extencion valida"
+        elif ext.upper() == "XML":
+            self.xml_string = self.xml_file.read()
+            self.success = True
+            self.message = "extencion valida"
+        else:
+            self.message = "El archivo no es un documento XML"
+
+    # funcion de validacion del ws de finkok
+    def validate_ws(self):
+        print("validacion ws")
+        self.success = False
+        lines = "".join(self.xml_file.readlines())
+        xml = lines.encode("UTF-8")
+        history = HistoryPlugin()
+        client = Client(wsdl=self.url, plugins=[history])
+        contenido = client.service.validate(self.xml_string, self.username, self.password)
+        try:
+            error = contenido.error
+            self.response = {'Estructura': self.message,
+                            'Sello': str(contenido.sello),
+                            'Sello_Sat':str(contenido.sello_sat),
+                            'Error': error}
+        except Exception:
+            print(contenido.sat)
+            print(contenido.sat.Estado)
+            print(contenido.sat.CodigoEstatus)
+            
+            
+        
+        
+        
+
+            
+       
+
+        
---
+++ validate/views.py
@@ -0,0 +1,54 @@
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, HttpResponse
+from django.views import View
+
+# importando libreria para responder en formato json.
+from django.http import JsonResponse
+
+
+# importando formulario para documentos
+from .forms import FileForm
+
+# importamos la clase Validate
+from .utils import Validate
+
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Create your views here.
+
+class IndexView(View):
+
+	def get(self, request):
+		#Obtenemos el formulario creado y lo mandamos a la vista.
+		form = FileForm()
+		return render(request, 'validate/index.html', {'form':form})
+
+	def post(self, request):
+		# Obtenemos el documento enviado
+		form = FileForm(request.POST, request.FILES)
+		if form.is_valid():
+			m = form.save()
+			# Obtenemos el file del request y lo amacenamos
+			xml_file = m.file
+			# mandamos el xml como parametro a la clase Validate para hacer el proceso de validacion.
+			validate = Validate(xml_file)
+			return JsonResponse(validate.response)
+
+       
+
+
+
+def layout(request):
+
+	return render(request, 'layout/layout.html')
+
+
+
+
+
+
+
+
